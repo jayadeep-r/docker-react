74eaaf033f932a57744242e466da53ad85fea413
jayadeep-r <jayadeep_r@yahoo.com>
2021-02-16 16:19:50 +0530

    Test change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-                   Welcome again !!
+                   Docker is fun !!
         </p>
         <a
           className="App-link"
